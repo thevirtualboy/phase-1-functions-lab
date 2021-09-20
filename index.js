2d2bd55b226eca5808769832e75759d78ab4cc6c
Peter Fontana <dude0508@gmail.com>
2021-09-20 18:03:50 -0400

    commit tree d0d02f6d9374b26212642475d7211fc67e41e64b

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickUpLocation) {
+    let blocks;
+
+    if (pickUpLocation <= 42) {
+        blocks = 42 - pickUpLocation;
+        return blocks;
+    } else if (pickUpLocation > 42) {
+        blocks = pickUpLocation - 42;
+        return blocks;
+    }
+}
+
+function distanceFromHqInFeet(pickUpLocation) {
+    return distanceFromHqInBlocks(pickUpLocation) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    if (start >= destination) {
+        return (start - destination) * 264;
+    } else if (start < destination) {
+        return (destination - start) * 264;
+    }
+}
+
+function calculatesFarePrice(start, destination) {
+    let farePrice;
+    let distance = distanceTravelledInFeet(start, destination);
+    if (distance <= 400) {
+        farePrice = 0;
+        return farePrice;
+    } else if (distance > 400 && distance <= 2000) {
+        farePrice = (distance - 400) * 0.02;
+        return farePrice;
+    } else if (distance > 2000 && distance <= 2500) {
+        farePrice = 25;
+        return farePrice;
+    } else if (distance > 2500) {
+        return 'cannot travel that far';
+    } 
+}
